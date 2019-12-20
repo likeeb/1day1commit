7b2cfd9b4aee80279d41f1f038b30df253ffc239
likeeb <k.eunbee@gmail.com>
2019-12-20 19:14:10 +0900

    백준 알고리즘 스텝1 문제2(10718)

---
+++ src/main/java/step_001/problem_002_10718.java
@@ -0,0 +1,8 @@
+package step_001;
+/**문제10718 | We love kriii**/
+public class problem_002_10718 {
+	public static void main (String [] args){
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+	}
+}
